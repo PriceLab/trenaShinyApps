79fc37f1b28f4612b9a5d895b08d309e7aba8809
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-08-15 17:53:34 -0700

    later() works, binding sites displayed per tf/transcript

--- mentewab/frd3/dataPrep/frd3.R
+++ mentewab/frd3/dataPrep/frd3.R
@@ -52,55 +52,127 @@ if(!exists("fc")){
    FIMO_PORT <- 5558
    fc <- FimoClient(FIMO_HOST, FIMO_PORT, quiet=FALSE)
    }
-if(!exists("tfs")){
-   pfms <- query(MotifDb, c("Athaliana", "jaspar2018"))
-   export(pfms, "~/github/fimoService/pfms/athaliana.all.meme", "meme")
-   start <- tss.orf.2 - 500
-   end   <- tss.orf.2 + 2000
+
+fimo.pval.threshold <- 5 * 10e-5
+
+if(!exists("candidate.tfs.1")){
+     # assemble and export pfms for fimo, if not already loaded and running
+     # pfms <- query(MotifDb, c("Athaliana", "jaspar2018"))
+     # export(pfms, "~/github/fimoService/pfms/athaliana.all.meme", "meme")
+   start <- tss.orf.1 - 499
+   end   <- tss.orf.1 + 2000
    sequence <- as.character(getSeq(BSgenome.Athaliana.TAIR.TAIR9, "Chr3", start, end))
    nchar(sequence)
-   tbl.fimo <- requestMatch(fc, list(frd3=sequence), pvalThreshold=0.0001)
+   tbl.fimo <- requestMatch(fc, list(frd3=sequence), pvalThreshold=fimo.pval.threshold)
    dim(tbl.fimo)
    tbl.fimo$start <- tbl.fimo$start + start
    tbl.fimo$stop <- tbl.fimo$stop + start
    tbl.fimo$chrom <- chromosome
-   tbl.bed <- tbl.fimo[, c("chrom", "start", "stop", "score", "motif")]
+   tbl.bed.1 <- tbl.fimo[, c("chrom", "start", "stop", "score", "motif")]
+   dim(tbl.bed.1)
    tbl.xtab <- as.data.frame(table(tbl.fimo$motif))
    fivenum(tbl.xtab$Freq)
    candidate.motifs <- unique(tbl.fimo$motif)
-   tfs <- as.data.frame(mcols(MotifDb[candidate.motifs]))$geneSymbol
-   tfs.orfs <- tbl.xref$orfs[match(tfs, tbl.xref$geneSymbol)]
+   tfs.1 <- as.data.frame(mcols(MotifDb[candidate.motifs]))$geneSymbol
+   tfs.orfs <- tbl.xref$orfs[match(tfs.1, tbl.xref$geneSymbol)]
    deleters <- which(is.na(tfs.orfs))
+   printf("%d/%d tf.orfs are NA", length(deleters), length(tfs.orfs))
    if(length(deleters) > 0)
       tfs.orfs <- tfs.orfs[-deleters]
-   candidate.tfs <- tfs.orfs
-   length(candidate.tfs)
+   candidate.tfs.1 <- tfs.orfs
+   length(candidate.tfs.1)
    } # tfs
 
-if(!exists("tbl.model")) {
-   solver <- EnsembleSolver(mtx, targetGene, candidate.tfs)
+if(!exists("candidate.tfs.2")){
+     # assemble and export pfms for fimo, if not already loaded and running
+     # pfms <- query(MotifDb, c("Athaliana", "jaspar2018"))
+     # export(pfms, "~/github/fimoService/pfms/athaliana.all.meme", "meme")
+   start <- tss.orf.2 - 499
+   end   <- tss.orf.2 + 2000
+   sequence <- as.character(getSeq(BSgenome.Athaliana.TAIR.TAIR9, "Chr3", start, end))
+   nchar(sequence)
+   tbl.fimo <- requestMatch(fc, list(frd3=sequence), pvalThreshold=fimo.pval.threshold)
+   dim(tbl.fimo)
+   tbl.fimo$start <- tbl.fimo$start + start
+   tbl.fimo$stop <- tbl.fimo$stop + start
+   tbl.fimo$chrom <- chromosome
+   tbl.bed.2 <- tbl.fimo[, c("chrom", "start", "stop", "score", "motif")]
+   dim(tbl.bed.2)
+   tbl.xtab <- as.data.frame(table(tbl.fimo$motif))
+   fivenum(tbl.xtab$Freq)
+   candidate.motifs <- unique(tbl.fimo$motif)
+   tfs.2 <- as.data.frame(mcols(MotifDb[candidate.motifs]))$geneSymbol
+   tfs.orfs <- tbl.xref$orfs[match(tfs.2, tbl.xref$geneSymbol)]
+   deleters <- which(is.na(tfs.orfs))
+   printf("%d/%d tf.orfs are NA", length(deleters), length(tfs.orfs))
+   if(length(deleters) > 0)
+      tfs.orfs <- tfs.orfs[-deleters]
+   candidate.tfs.2 <- tfs.orfs
+   length(candidate.tfs.2)
+   } # tfs
+
+if(!exists("tbl.model.1")) {
+   solver <- EnsembleSolver(mtx, targetGene, candidate.tfs.1)
    tbl.model <- run(solver)
    tbl.model <- tbl.model[order(tbl.model$rfScore, decreasing=TRUE),]
    tbl.model$symbol <- tbl.xref[match(tbl.model$gene, tbl.xref$orfs), "geneSymbol"]
    tbl.model <- tbl.model[order(tbl.model$pcaMax, decreasing=TRUE),]
    tbl.model <- roundNumericColumnsInDataframe(tbl.model, 3, "lassoPValue")
    tbl.model <- subset(tbl.model, abs(pearsonCoeff) > 0.4)
-   save(tbl.model, file="../shinyApp/data/tbl.model.frd3-transcript2.RData")
-
+   tbl.model.1 <- tbl.model
+   save(tbl.model.1, file="../shinyApp/data/tbl.model.frd3-transcript1.RData")
    }  # tbl.model
 
-if(!exists("tbl.bindingSites")){
-   tbl.bindingSites <- unique(subset(tbl.bed, motif %in% tbl.xref[match(tbl.model$gene, tbl.xref$orfs), "motif"]))
-   geneSymbols <- tbl.xref$geneSymbol[match(tbl.bindingSites$motif, tbl.xref$motif)]
-   tbl.bindingSites$geneSymbol <- geneSymbols
-   tfs.unique <- sort(unique(tbl.bindingSites$geneSymbol))
+if(!exists("tbl.model.2")) {
+   solver <- EnsembleSolver(mtx, targetGene, candidate.tfs.2)
+   tbl.model <- run(solver)
+   tbl.model <- tbl.model[order(tbl.model$rfScore, decreasing=TRUE),]
+   tbl.model$symbol <- tbl.xref[match(tbl.model$gene, tbl.xref$orfs), "geneSymbol"]
+   tbl.model <- tbl.model[order(tbl.model$pcaMax, decreasing=TRUE),]
+   tbl.model <- roundNumericColumnsInDataframe(tbl.model, 3, "lassoPValue")
+   tbl.model <- subset(tbl.model, abs(pearsonCoeff) > 0.4)
+   tbl.model.2 <- tbl.model
+   save(tbl.model.2, file="../shinyApp/data/tbl.model.frd3-transcript2.RData")
+   }  # tbl.model
+
+if(!exists("tbl.bindingSites.1")){
+   tbl.bindingSites.1 <- unique(subset(tbl.bed.1, motif %in% tbl.xref[match(tbl.model$gene, tbl.xref$orfs), "motif"]))
+   geneSymbols <- tbl.xref$geneSymbol[match(tbl.bindingSites.1$motif, tbl.xref$motif)]
+   tbl.bindingSites.1$geneSymbol <- geneSymbols
+   tfs.unique <- sort(unique(tbl.bindingSites.1$geneSymbol))
    for(tf in tfs.unique){
-      tbl.tfbs <- subset(tbl.bindingSites, geneSymbol==tf)
+      tbl.tfbs <- subset(tbl.bindingSites.1, geneSymbol==tf)
       track <- DataFrameAnnotationTrack(tf, tbl.tfbs, color="red", trackHeight=25)
       displayTrack(igv, track)
       } # for tf
-   save(tbl.bindingSites, file="../shinyApp/data/tbl.bindingSites.frd3-transcript2.RData")
-   } # tbl.bindingSites
+   save(tbl.bindingSites.1, file="../shinyApp/data/tbl.bindingSites.1.frd3-transcript2.RData")
+   } # tbl.bindingSites.1
+
+if(!exists("tbl.bindingSites.1")){
+   tbl.bindingSites.1 <- unique(subset(tbl.bed.1, motif %in% tbl.xref[match(tbl.model$gene, tbl.xref$orfs), "motif"]))
+   geneSymbols <- tbl.xref$geneSymbol[match(tbl.bindingSites.1$motif, tbl.xref$motif)]
+   tbl.bindingSites.1$geneSymbol <- geneSymbols
+   tfs.unique <- sort(unique(tbl.bindingSites.1$geneSymbol))
+   for(tf in tfs.unique){
+      tbl.tfbs <- subset(tbl.bindingSites.1, geneSymbol==tf)
+      track <- DataFrameAnnotationTrack(tf, tbl.tfbs, color="red", trackHeight=25)
+      displayTrack(igv, track)
+      } # for tf
+   save(tbl.bindingSites.1, file="../shinyApp/data/tbl.bindingSites.frd3-transcript1.RData")
+   } # tbl.bindingSites.1
+
+if(!exists("tbl.bindingSites.2")){
+   tbl.bindingSites.2 <- unique(subset(tbl.bed.2, motif %in% tbl.xref[match(tbl.model$gene, tbl.xref$orfs), "motif"]))
+   geneSymbols <- tbl.xref$geneSymbol[match(tbl.bindingSites.2$motif, tbl.xref$motif)]
+   tbl.bindingSites.2$geneSymbol <- geneSymbols
+   tfs.unique <- sort(unique(tbl.bindingSites.2$geneSymbol))
+   for(tf in tfs.unique){
+      tbl.tfbs <- subset(tbl.bindingSites.2, geneSymbol==tf)
+      track <- DataFrameAnnotationTrack(sprintf("%s.2", tf), tbl.tfbs, color="red", trackHeight=22)
+      displayTrack(igv, track)
+      } # for tf
+   save(tbl.bindingSites.2, file="../shinyApp/data/tbl.bindingSites.frd3-transcript2.RData")
+   } # tbl.bindingSites.2
 
 
 
--- mentewab/frd3/shinyApp/app.R
+++ mentewab/frd3/shinyApp/app.R
@@ -6,9 +6,13 @@ library(GenomicRanges)
 library(later)
 library(MotifDb)
 library(RUnit)
+library(RColorBrewer)
+#----------------------------------------------------------------------------------------------------
+totalColorCount <- 12
+colors <- brewer.pal(totalColorCount, "Paired")
+colorNumber <- 0
 #----------------------------------------------------------------------------------------------------
 load("data/tbls.frd3.dhs.budAndLeaf.RData")   # "tbl.frd3buds" "tbl.frd3leaf"
-
 tbl.buds <- tbl.frd3buds
 tbl.buds$sampleID <- paste("seg", seq_len(nrow(tbl.buds)), sep=".")
 tbl.buds$chrom <- rep("3", nrow(tbl.buds))
@@ -18,8 +22,12 @@ tbl.leaf <- tbl.frd3leaf
 tbl.leaf$sampleID <- paste("seg", seq_len(nrow(tbl.leaf)), sep=".")
 tbl.leaf$chrom <- rep("3", nrow(tbl.leaf))
 colnames(tbl.leaf) <- c("chr", "start", "end", "value", "sampleID")
+
+load("data/tbl.bindingSites.frd3-transcript1.RData") #  "tbl.bindingSites.1"
+load("data/tbl.bindingSites.frd3-transcript2.RData") #  "tbl.bindingSites.2
 #----------------------------------------------------------------------------------------------------
-load("data/tbl.model.frd3-transcript2.RData")  # tbl.model
+load("data/tbl.model.frd3-transcript1.RData")  # tbl.model.1
+load("data/tbl.model.frd3-transcript2.RData")  # tbl.model.2
 #----------------------------------------------------------------------------------------------------
 geneSymbol <- "FRD3"
 orf <- "AT3G08040"
@@ -44,14 +52,23 @@ ui <- fluidPage(
                       "AT3G08040.1 promoter" = "transcript.1.promoter",
                       "AT3G08040.2 promoter" = "transcript.2.promoter")),
 
-        selectInput("geneModel", "Choose model:",
-                    c("transcript 2", "transcript.2.model")),
+        selectInput("geneModel", "Choose regulatory model:",
+                    c(" - " = "no.model",
+                      "transcript 1"="transcript.1.model",
+                      "transcript 2"="transcript.2.model")),
 
         selectInput("addTrack", "Add Track:",
                    c(" - ",
                      "bud DHS"="bud.dhs",
                      "leaf DHS"="leaf.dhs"
+                     )),
+
+        selectInput("addBindingSites", "Add TF binding sites for regulatory model:",
+                   c(" - ",
+                     "transcript 1"="transcript.1.model",
+                     "transcript 2"="transcript.2.model"
                      ))
+
        ),  # sidebarPanel
 
      mainPanel(
@@ -74,12 +91,10 @@ ui <- fluidPage(
 #--------------------------------------------------------------------------------
 server <- function(input, output, session) {
 
-   session$sendCustomMessage(type="showGenomicRegion", message=(list(region="FLT1")))
    dt.proxy <- dataTableProxy("geneModelTable")
 
    observeEvent(input$displayGenomicRegion, {
       regionName <- input$displayGenomicRegion
-      printf("display event: %s", regionName)
       regions <- list(fullgene="3:2,565,078-2,572,981",
                       transcript.1.promoter=sprintf("%s:%d-%d", chromosome, tss.orf.1-499, tss.orf.1+2000),
                       transcript.2.promoter=sprintf("%s:%d-%d", chromosome, tss.orf.2-499, tss.orf.2+2000)
@@ -100,30 +115,70 @@ server <- function(input, output, session) {
    observeEvent(input$addTrack, {
       trackName <- input$addTrack
       printf("addTrack: %s", trackName)
+      updateTabsetPanel(session, "trenaTabs", select="igvTab");
+      if(trackName == " - ") return();
       tbl.dhs <- switch(trackName, "bud.dhs"=tbl.buds, "leaf.dhs"=tbl.leaf)
-      loadBedGraphTrack(trackName, tbl.dhs)
+      myFunc <- function() loadBedGraphTrack(session, trackName, tbl.dhs)
+      later(myFunc, delay=1);
+      })
+
+   observeEvent(input$addBindingSites, ignoreInit= TRUE, {
+      modelName <- input$addBindingSites;
+      printf("addBindingSites: %s", modelName);
+      tbl.bs <- switch(modelName, "transcript.1.model" = tbl.bindingSites.1, "transcript.2.model" = tbl.bindingSites.2)
+      transcript.number <- switch(modelName, "transcript.1.model" = 1, "transcript.2.model" = 2)
+      tbl.bs <- tbl.bs[, c("chrom", "start", "stop", "score", "motif", "geneSymbol")]
+      colnames(tbl.bs) <- c("chr", "start", "end", "value", "sampleID", "geneSymbol")
+      printf("loadingBedGraphTrack, dim: %d %d", nrow(tbl.bs), ncol(tbl.bs))
+      myFunc <- function(){
+         printf("entering myFunc, rows in tbl: %d", nrow(tbl.bs));
+         tfs <- sort(unique(tbl.bs$geneSymbol))
+         printf(" %d tfs", length(tfs))
+         for(tf in tfs){
+            colorNumber <<- (colorNumber + 1) %% totalColorCount
+            tbl.bs.sub <- subset(tbl.bs, geneSymbol==tf)
+            track.name <- sprintf("%s.%d", tf, transcript.number)
+            loadBedTrack(session, track.name, tbl.bs.sub, color=colors[colorNumber], trackHeight=25)
+            } # for tf
+         }
+        # myFunc <- function() loadBedGraphTrack(session, modelName, tbl.bs, color=colors[colorNumber], trackHeight=30)
+      updateTabsetPanel(session, "trenaTabs", select="igvTab");
+        # allow time for the igv tab to be displayed
+      later(myFunc, delay=1);
       })
 
    output$igvShiny <- renderIgvShiny({
-     igvShiny(list(genomeName="tair10", initialLocus=sprintf("%s:%d-%d", chromosome, loc.start, loc.end)))
+     printf("rendering igvShiny");
+     igvShiny(list(genomeName="tair10",
+                   initialLocus=sprintf("%s:%d-%d", chromosome, loc.start, loc.end)))
      })
+
+
+   observeEvent(input$geneModel, ignoreInit = TRUE, {
+      model.name <- isolate(input$geneModel);
+      if(model.name != " - "){
+         printf("geneModel menu changed: %s", model.name)
+         updateTabsetPanel(session, "trenaTabs", select="geneModelTab")
+         }
+      })
 
    output$geneModelTable <- renderDT({
       model.name <- input$geneModel;
-      updateTabsetPanel(session, "trenaTabs", select="geneModelTab");
-      printf("    rendering DT, presumably because input$geneModel changes, model.name: %s", model.name);
-      selection="single"
-      tbl.model <- tbl.model
-      printf("data.frame dimensions: %d, %d", nrow(tbl.model), ncol(tbl.model))
-      if(nrow(tbl.model) > 20)
-         tbl.model <- tbl.model[1:20,]
-      rownames(tbl.model) <- NULL
-      #state[["currentModel"]] <- tbl.model
-      #state[["currentModelName"]] <- model.name
-      return(tbl.model)
-      },
-    selection = 'single',
-    options=list(pageLength=25, dom="t"))
+      if(model.name != "no.model"){
+         printf("    rendering DT, presumably because input$geneModel changes, model.name: %s", model.name);
+         selection="single"
+         tbl.model <- switch(model.name, "transcript.1.model"=tbl.model.1, "transcript.2.model"=tbl.model.2)
+         printf("data.frame dimensions: %d, %d", nrow(tbl.model), ncol(tbl.model))
+         if(nrow(tbl.model) > 10)
+            tbl.model <- tbl.model[1:10,]
+         rownames(tbl.model) <- NULL
+         # later(function() {updateTabsetPanel(session, "trenaTabs", select="geneModelTab")}, 1);
+         return(tbl.model)
+         } # not "no.model"
+     },
+     selection = 'single',
+     options=list(pageLength=25, dom="t")
+     ) # renderDT
 
   } # server
 
