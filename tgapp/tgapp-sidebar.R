fbd73666fb76d38c58fcd15624737fb41c433f0c
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-10-12 12:15:00 -0700

    stripped out all unused code. about to add trenaSGM tab

--- tgapp/tgapp-sidebar.R
+++ tgapp/tgapp-sidebar.R
@@ -17,6 +17,15 @@ library(trenaSGM)
 library(TrenaGene)
 library(TrenaGenePlacentaData)
 #------------------------------------------------------------------------------------------------------------------------
+targetGene <- list(hugo="PSG1", ensembl="ENSG00000231924", combined="ENSG00000231924|PSG1")
+genomeName <- "hg38"
+dataDirectory <- system.file(package="TrenaGenePlacentaData", "extdata")
+
+trenaGene <- TrenaGene(targetGene$hugo, genomeName, expressionDataDirectory=dataDirectory)
+expression.data.set.names <- getExpressionMatrixNames(trenaGene)
+tbl.enhancers <- getEnhancers(trenaGene)
+tbl.dhs <- getEncodeDHS(trenaGene)
+#------------------------------------------------------------------------------------------------------------------------
 combineGeneNames <- function(ensemblIDs)
 {
    tbl.geneNameMapper <- select(org.Hs.eg.db, keys=ensemblIDs, keytype="ENSEMBL", columns="SYMBOL")
@@ -27,24 +36,10 @@ combineGeneNames <- function(ensemblIDs)
   dups <- which(duplicated(tbl.geneNameMapper$ENSEMBL)) # 167
   if(length(dups) > 0)
      tbl.geneNameMapper <- tbl.geneNameMapper[-dups,]
-  #dim(tbl.geneNameMapper); dim(mtx)
-   #rownames(mtx) <- tbl.geneNameMapper$combined
    return(tbl.geneNameMapper)
 
 } # combineGeneNames
 #------------------------------------------------------------------------------------------------------------------------
-targetGene <- list(hugo="PSG1", ensembl="ENSG00000231924", combined="ENSG00000231924|PSG1")
-genomeName <- "hg38"
-
-dataDirectory <- system.file(package="TrenaGenePlacentaData", "extdata")
-
-trenaGene <- TrenaGene(targetGene$hugo, genomeName, expressionDataDirectory=dataDirectory)
-expression.data.set.names <- getExpressionMatrixNames(trenaGene)
-tbl.enhancers <- getEnhancers(trenaGene)
-tbl.dhs <- getEncodeDHS(trenaGene)
-
-
-#----------------------------------------------------------------------------------------------------
 totalColorCount <- 6
 colors <- brewer.pal(totalColorCount, "Dark2")
 colorNumber <- 0
@@ -52,7 +47,6 @@ colorNumber <- 0
 ui <- fluidPage(
 
   tags$script(JS('setInterval(function(){console.log("keepAlive"); Shiny.setInputValue("foo", "var");}, 1 * 60 * 1000)')),
-
   titlePanel(sprintf("%s Trena Model & Disruptions", targetGene$hugo)),
 
   sidebarLayout(
@@ -65,8 +59,7 @@ ui <- fluidPage(
         selectInput("addTrack", "Add Track:",
                    c("",
                      "Enhancers"="enhancers",
-                     "DHS open chromatin - encode clustered"="dhs",
-                     "ChIP-seq"="chipSeq"
+                     "DHS open chromatin - encode clustered"="dhs"
                      )),
         HTML("<br>"),
         actionButton("removeOptionalTracks", "Remove tracks")
@@ -95,18 +88,8 @@ ui <- fluidPage(
 #--------------------------------------------------------------------------------
 server <- function(input, output, session) {
 
-   #session$sendCustomMessage(type="showGenomicRegion", message=(list(region="FLT1")))
+
    dt.proxy <- dataTableProxy("geneModelTable")
-   #later(function() {
-   #   printf("about to select gene family");
-   #   updateSelectInput(session, "displayGenomicRegion", selected="overview")
-   #   }, 3)
-
-    # define a reactive conductor. it returns a function, is
-    # redefined with a change to dist or n
-    # has reactive children  plot, summary, table, below, which mention it
-    # thereby establishing the reactive chain.
-
 
    observeEvent(input$trackClick, {
       printf("browser snp click observed!")
@@ -136,23 +119,9 @@ server <- function(input, output, session) {
          } # if tfbs-snp
       })
 
-   observeEvent(input$addAllBindingSitesButton, ignoreInit=TRUE,{
-      addAllBindingSites(session)
-     })
-
    observeEvent(input$removeOptionalTracks, ignoreInit = TRUE, {
       removeUserAddedTracks(session)
       })
-
-   observeEvent(input$geneModel, {
-      printf("geneModel event: %s", input$geneModel);
-      updateTabsetPanel(session, "trenaTabs", select="geneModelTab");
-      })
-
-   observeEvent(input$showTargetGeneButton, {
-     roi <- "chr6:41,152,337-41,213,272"
-     # session$sendCustomMessage(type="showGenomicRegion", message=(list(region=roi)))
-     })
 
    observeEvent(input$displayGenomicRegion, ignoreInit=TRUE,  {
       regionName <- isolate(input$displayGenomicRegion)
@@ -176,92 +145,6 @@ server <- function(input, output, session) {
       later(function() {updateSelectInput(session, "addTrack", selected=character(0))}, 1)
       })
 
-   observeEvent(input$findDisruptiveSNPsButton, {
-      snpSignificanceThreshold <- isolate(session$input$IGAP.snp.significance)
-      motifMatchThreshold <- isolate(session$input$fimo.motif.significance)
-      bindingLossThreshold <- snpEffect.pval.threshold <- isolate(session$input$fimo.snp.effect)
-      tbl.disruptions <- findDisruptiveSNPs(snpSignificanceThreshold, motifMatchThreshold, bindingLossThreshold)
-      if(nrow(tbl.disruptions) > 0)
-         displayDisruptiveSnps(session, tbl.disruptions)
-      })
-
-   observeEvent(input$findSnpsInModelButton, {
-      trena.model <- isolate(input$model.trn)
-      filters <- isolate(input$filters)
-      snpShoulder <- isolate(input$snpShoulder)
-      filter.result <- applyFilters(session, filters, trena.model, snpShoulder)
-      })
-
-   observeEvent(input$pwmMatchThreshold, {
-      printf("  ** pwmMatchThreshold changed");
-      currentPwmMatchThreshold <- input$pwmMatchThreshold
-      })
-
-   observeEvent(input$filters, {
-      currentFilters <- input$filters
-      print(paste(currentFilters, collapse=","))
-      })
-
-   observeEvent(input$model.trn, {
-      currentModel <- input$model.trn
-      print(paste(currentFilters, collapse=","))
-      })
-
-   observeEvent(input$roi, {
-     currentValue = input$roi
-     tss <- 88884466;   # get from MEF2C.data
-     roi.string <- switch(currentValue,
-                          tss_2kb = sprintf("chr5:%d-%d", tss-2000, tss+2000),
-                          tss_5kb = sprintf("chr5:%d-%d", tss-5000, tss+5000),
-                          studyRegion = "chr5:88391000-89322000")
-     session$sendCustomMessage(type="roi", message=(list(roi=roi.string)))
-     })
-
-   observeEvent(input$showSNPsNearBindingSitesButton, {
-      displaySNPsNearBindingSites(session)
-      })
-
-   observeEvent(input$geneModelTable_rows_selected, {
-         selectedTableRow <- isolate(input$geneModelTable_rows_selected)
-         tbl.model <- state$currentModel
-         tf <- tbl.model$tf.hgnc[selectedTableRow]
-         printf("%d %s", selectedTableRow, tf)
-         tfSelectionAction <- isolate(input$tfSelectionChoice)
-         printf("table row clicked: %s  - actionRequested: %s", tf, tfSelectionAction);
-         if(tfSelectionAction == "xyPlot"){
-            updateTabsetPanel(session, "trenaTabs", select="plotTab");
-            #tokens <- strsplit(state$currentModelName, "\\.")[[1]]
-            #expression.matrix.id <- tokens[length(tokens)]
-            expression.matrix.id <- "tcx"
-            output$xyPlot = renderPlot(plotTfTargetGeneCorrelation(session, tf, expression.matrix.id))
-             }
-         if(tfSelectionAction == "displayBindingSites"){
-            displayBindingSites(session, tf)
-            }
-         }) # observe row selection event
-
-   output$geneModelTable <- renderDT(
-      {model.name <- input$geneModel;
-       updateTabsetPanel(session, "trenaTabs", select="geneModelTab");
-       printf("    rendering DT, presumably because input$geneModel changes, model.name: %s", model.name);
-       selection="single"
-       #tbl.model <- tbl.model[, c("tf.hgncall.models[[model.name]]$model
-       printf("data.frame dimensions: %d, %d", nrow(tbl.model), ncol(tbl.model))
-       #tbl.model <- switch(model.name,
-       #   "Enhancers.TFClass" = tbl.model.enhancerAll.tfc,
-       #   "Enhancers.MotifDb" = tbl.model.enhancerAll.mdb
-       #   )
-      #tbl.model <- tbl.model[order(tbl.model$rfScore, decreasing=TRUE),]
-      if(nrow(tbl.model) > 20)
-         tbl.model <- tbl.model[1:20,]
-      rownames(tbl.model) <- NULL
-      state[["currentModel"]] <- tbl.model
-      state[["currentModelName"]] <- model.name
-      return(tbl.model)
-      },
-    selection = 'single',
-    options=list(pageLength=25, dom="t"))
-
    output$igvShiny <- renderIgvShiny({
      igvShiny(list(genomeName=genomeName,
                    initialLocus=targetGene$hugo))
@@ -273,36 +156,17 @@ server <- function(input, output, session) {
 displayTrack <- function(session, trackName)
 {
    supportedTracks <- c("enhancers",
-                        "dhs",
-                        "chipSeq")
+                        "dhs")
 
    printf("--- displayTrack('%s')", trackName)
 
    switch(trackName,
-          enhancers = {printf("create enhancers track");
-                       displayEnhancersTrack(session)},
-          dhs       = {printf("create dhs track");
-                       displayEncodeDhsTrack(session)},
-                       # c("chrom", "chromStart", "chromEnd", "count", "score"))
-
-          chipSeq   = {printf("create ChIP-seq track")}
+          enhancers = {displayEnhancersTrack(session)},
+          dhs       = {displayEncodeDhsTrack(session)}
           )
 
     updateTabsetPanel(session, "trenaTabs", select="igvTab");
 
-#    if(trackName == "snps"){
-#       tbl.tmp <- x$bedgraph[, c("chrom", "start", "end", "pScore")]
-#       loadBedGraphTrack(session, "GWAS snp", tbl.tmp, color=x$color, trackHeight=25, autoscale=FALSE, min=0, max=10)
-#       } # bedGraph for snps
-#
-#    if(trackName == "dhs"){
-#       loadBedGraphTrack(session, "DHS", x$bedgraph, color=x$color, trackHeight=25, autoscale=TRUE)
-#       } # bedGraph for snps
-#
-#
-#   if(trackName %in% c("enhancer.snps", "breaking.snps", "chipSeq", "rs35349669", "spi1.footprints")){
-#       loadBedTrack(session, trackName, x$bed, color=x$color, trackHeight=30)
-#       } # bed format track
 
 } # displayTrack
 #------------------------------------------------------------------------------------------------------------------------
@@ -320,151 +184,6 @@ displayEncodeDhsTrack <- function(session)
    loadBedGraphTrack(session, "DHS", tbl.tmp, color="black", trackHeight=25, autoscale=TRUE)
 
 } # displayEncodeDhsTrack
-#------------------------------------------------------------------------------------------------------------------------
-displayChipSeqByTF <- function(session)
-{
-   colors <- brewer.pal(8, "Dark2")
-   totalColorCount <- length(colors)
-   colorNumber <- 0
-
-   for(gene in unique(tbl.csInModel$tf)){
-      tbl.broad <- subset(tbl.csInModel, tf==gene)[, c("chr", "start", "end")]
-      tbl.peak <- subset(tbl.csInModel, tf==gene)[, c("chr", "peakStart", "peakEnd")]
-      colnames(tbl.peak) <- c("chr", "start", "end")
-      colorNumber <- (colorNumber %% totalColorCount) + 1
-      color <- colors[colorNumber]
-      loadBedTrack(session, sprintf("%s ChIP", gene), tbl.broad, color=color, trackHeight=25)
-      # loadBedTrack(session, sprintf("%s peak", gene), tbl.peak, color=color, trackHeight=25)
-      } # for tf
-
-} # displayChipSeqByTF
-#------------------------------------------------------------------------------------------------------------------------
-# tbl.pheno$Diagnosis distribution in temporal cortex data
-#   AMP-AD_MayoBB_UFL-Mayo-ISB_IlluminaHiSeq2000_TCX_Covariates_Flowcell_1.csv
-#
-# cerebellum phenotypes not yet found  (11 jun 2018)
-#
-#                AD   84
-#           Control   80
-#  Pathologic Aging   30
-#               PSP   84
-#
-plotTfTargetGeneCorrelation <- function(session, tf, expression.matrix.id)
-{
-   stopifnot(expression.matrix.id %in% c("cer", "tcx"))
-   mtx <- switch(expression.matrix.id,
-                 cer = mtx.cer,
-                 tcx = mtx.tcx
-                 )
-   printf("want an xyplot of %s vs. INPP5D", tf)
-   covariates.file <- "data/AMP-AD_MayoBB_UFL-Mayo-ISB_IlluminaHiSeq2000_TCX_Covariates_Flowcell_1.csv"
-   tbl.pheno <- read.table(covariates.file, sep=",", as.is=TRUE, header=TRUE)
-
-   if(expression.matrix.id == "tcx"){ # we have phenotype data
-      samples.AD <- intersect(subset(tbl.pheno, Diagnosis=="AD")$ID, colnames(mtx))
-      samples.ctl <- intersect(subset(tbl.pheno, Diagnosis=="Control")$ID, colnames(mtx))
-      samples.pathAging <- intersect(subset(tbl.pheno, Diagnosis=="Pathologic Aging")$ID, colnames(mtx))
-      samples.PSP  <- intersect(subset(tbl.pheno, Diagnosis=="PSP")$ID, colnames(mtx))
-      samples.all <- colnames(mtx)
-
-      color.choices <- list(AD="red",
-                            pathAging="green",
-                            PSP="blue",
-                            control="black"
-                            )
-
-      colors <- rep("black", length(samples.all))
-      colors[match(samples.AD, colnames(mtx))] <- color.choices$AD
-      colors[match(samples.ctl, colnames(mtx))] <- color.choices$control
-      colors[match(samples.pathAging, colnames(mtx))] <- color.choices$pathAging
-      colors[match(samples.PSP, colnames(mtx))] <- color.choices$PSP
-
-      plot(mtx[tf, samples.all], mtx[targetGene, samples.all],main="Gene Expression - Temporal Cortex",
-           xlab=tf, ylab=targetGene, col=colors, pch=19, xlim=c(-3,3), ylim=c(-3,3))
-
-      tbl.AD <- as.data.frame(t(mtx[c(tf, targetGene), samples.AD]))
-      model.AD <- lm(sprintf("%s ~ %s", targetGene, tf), data=tbl.AD)
-      abline(model.AD, col=color.choices$AD)
-
-      tbl.control <- as.data.frame(t(mtx[c(tf, targetGene), samples.ctl]))
-      model.control <- lm(sprintf("%s ~ %s", targetGene, tf), data=tbl.control)
-      abline(model.control, col=color.choices$control)
-
-      #browser()
-
-      tbl.pathAging <- as.data.frame(t(mtx[c(tf, targetGene), samples.pathAging]))
-      model.pathAging <- lm(sprintf("%s ~ %s", targetGene, tf), data=tbl.pathAging)
-      abline(model.pathAging, col=color.choices$pathAging)
-
-      tbl.PSP <- as.data.frame(t(mtx[c(tf, targetGene), samples.PSP]))
-      model.PSP <- lm(sprintf("%s ~ %s", targetGene, tf), data=tbl.PSP)
-      abline(model.PSP, col=color.choices$PSP)
-
-      legend.names <- names(color.choices)
-      for(i in seq_len(length(legend.names))){
-         condition <- legend.names[i]
-         rsq <- eval(parse(text=sprintf("summary(model.%s)$r.squared", condition)))
-         legend.names [i] <- sprintf("%s (rSq=%5.3f)", condition, rsq)
-         }
-      legend (1.5, -1.8, legend.names, as.character(color.choices))
-      }
-   else{  # no phenotype data, use all samples
-      tf.vals <- mtx[tf, ]
-      targetGene.vals <- mtx[targetGene,]
-      plot(tf.vals, targetGene.vals, main="Gene Expression - Cerebellum", xlab=tf, ylab=targetGene)
-      }
-
-
-} # plotTfTargetGeneCorrelation
-#------------------------------------------------------------------------------------------------------------------------
-displaySNPsNearBindingSites <- function(session)
-{
-   shoulder <- isolate(session$input$snpShoulder)
-   snpSignificanceThreshold <- isolate(session$input$IGAP.snp.significance)
-   motifMatchThreshold <- isolate(session$input$fimo.motif.significance)
-
-   printf("--- displaySNPsNearBindingSites")
-   printf("shoulder: %d", shoulder)
-   printf("snpSig: %f", snpSignificanceThreshold)
-   printf("motif match: %f", motifMatchThreshold)
-
-   tbl.snpFiltered <- subset(tbl.snp, pScore >= snpSignificanceThreshold)
-   if(nrow(tbl.snpFiltered) == 0){
-      printf("no snps above pScore threshold")
-      return()
-      }
-
-   tbl.tfbs <- subset(tbl.fimo, motif.pScore >= motifMatchThreshold)
-   if(nrow(tbl.tfbs) == 0){
-      printf("no binding sites above fimoe threshold")
-      return()
-      }
-
-   tbl.tfbs <- tbl.tfbs[, c("chrom", "motifStart", "motifEnd", "motif.pScore", "motifName")]
-   colnames(tbl.tfbs) <- c("chrom", "start", "end", "score", "motifName")
-   tbl.snpsNearEnough <- data.frame()
-   gr.snps <- GRanges(tbl.snp)
-   gr.tfbs <- GRanges(data.frame(chrom=tbl.tfbs$chrom,
-                                 start=tbl.tfbs$start - shoulder,
-                                 end=tbl.tfbs$end+shoulder))
-   tbl.ov <- as.data.frame(findOverlaps(gr.snps, gr.tfbs))
-   if(nrow(tbl.ov) == 0){
-      printf("no snps near binding sites")
-      return()
-      }
-
-   colnames(tbl.ov) <- c("snp", "tfbs")
-   tbl.snpsNearEnough <- tbl.snp[tbl.ov$snp, c("chrom", "start", "end", "rsid")]
-   motifNames <- tbl.tfbs$motifName[tbl.ov$tfbs]
-   tbl.snpsNearEnough$motifName <- motifNames
-   rownames(tbl.snpsNearEnough) <- NULL
-   tbl.snpsNearEnough <- unique(tbl.snpsNearEnough)
-   tbl.snpsNearEnough$name <- with(tbl.snpsNearEnough, paste(rsid, motifName, sep=":"))
-   tbl.snpsNearEnough <- tbl.snpsNearEnough[, c("chrom", "start", "end", "name")]
-
-   loadBedTrack(session, "snps near binding site", tbl.snpsNearEnough, color="brown", trackHeight=25)
-
-} # displaySNPsNearBindingSites
 #------------------------------------------------------------------------------------------------------------------------
 mapToChromLoc <- function(regionName)
 {
